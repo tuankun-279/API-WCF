82dda1a509f80efaf9ee7c585c418c4779f467b4
tuankun-279 <59610132+tuankun-279@users.noreply.github.com>
2021-09-19 15:51:43 -0700

    abc

---
+++ GetStudent/Controllers/GetStudentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetStudent.Controllers
+{
+    public class GetStudentController : Controller
+    {
+        public static ServiceReferenceGetStudent.Service1Client myService = new ServiceReferenceGetStudent.Service1Client();
+        // GET: GetStudent
+        public ActionResult Index()
+        {            
+            ViewBag.ListSinhVien = myService.Get_ListSinhVien();
+            return View();
+        }
+        public ActionResult Create(string Name, string Email)
+        {            
+            ServiceReferenceGetStudent.SinhVien sinhvien = new ServiceReferenceGetStudent.SinhVien();
+            if(!String.IsNullOrEmpty(Name) && !String.IsNullOrEmpty(Email))
+            {
+                sinhvien.Name = Name;
+                sinhvien.Email = Email;
+                myService.Insert_ListSinhVien(sinhvien);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ GetStudent/Controllers/JsonGetStudentController.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Helpers;
+using System.Web.Http;
+
+namespace GetStudent.Controllers
+{
+    public class JsonGetStudentController : ApiController
+    {
+        public static ServiceReferenceGetStudent.Service1Client myService = new ServiceReferenceGetStudent.Service1Client();
+        public JToken GetStudent()
+        {
+            return JToken.FromObject(myService.Get_ListSinhVien());
+        }
+        public void AddStudent(ServiceReferenceGetStudent.SinhVien sinhVien)
+        {
+            myService.Insert_ListSinhVien(sinhVien);           
+        }
+    }
+}
